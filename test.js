adb66401ebeef5eed4c5d18eb1af92656945378c
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-14 19:17:08 +0200

    Bump AVA

--- test.js
+++ test.js
@@ -2,15 +2,9 @@ import execa from 'execa';
 import test from 'ava';
 
 test('should return true', async t => {
-	const exec = await execa('./cli.js', ['fixtures/optipng']);
-	t.is(exec.stdout, 'true');
+	t.is((await execa('./cli.js', ['fixtures/optipng'])).stdout, 'true');
 });
 
 test('should return exit code 1', async t => {
-	try {
-		await execa('./cli.js', ['readme.md']);
-		t.fail();
-	} catch (err) {
-		t.is(err.code, 1);
-	}
+	t.throws(execa('./cli.js', ['readme.md']));
 });
